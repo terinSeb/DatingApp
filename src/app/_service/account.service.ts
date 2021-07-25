597c5b234327289794e8e7746021a6b69cd4341e
Terin Sebastin <terin01@gmail.com>
2021-07-25 17:47:33 +0530

    Added Logout Functionality

--- src/app/_service/account.service.ts
+++ src/app/_service/account.service.ts
@@ -1,9 +1,13 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
-
-  constructor() { }
+baseUrl ='https://localhost:44336/api/'
+  constructor(private http:HttpClient) { }
+  login(model:any){
+    return this.http.post(this.baseUrl +'account/login',model);
+  }
 }
--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../_service/account.service';
 
 @Component({
   selector: 'app-nav',
@@ -6,13 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
-model: any ={}
-  constructor() { }
+model: any ={};
+loggedIn: Boolean;
+  constructor(private accounServive: AccountService) { }
 
   ngOnInit(): void {
   }
   login(){
-    console.log(this.model);
+    this.accounServive.login(this.model).subscribe(response => {
+      console.log(response);
+      this.loggedIn = true;
+    }, error => {
+      console.log(error);
+    })
+  }
+  logout(){
+    this.loggedIn = false;
   }
 
 }
