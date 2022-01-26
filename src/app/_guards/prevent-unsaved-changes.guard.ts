551d171c55f341cfdf12572a0588b29273ed8c0f
Terin Sebastin <terin01@gmail.com>
2022-01-26 21:02:58 +0530

    Corrected canDeactivate Bug

--- src/app/_guards/prevent-unsaved-changes.guard.ts
+++ src/app/_guards/prevent-unsaved-changes.guard.ts
@@ -13,6 +13,7 @@ import { MemberEditComponent } from '../members/member-edit/member-edit.componen
 })
 export class PreventUnsavedChangesGuard implements CanDeactivate<unknown> {
   canDeactivate(component: MemberEditComponent): boolean {
+    // console.log(component.editForm.submitted);
     if (component.editForm.dirty) {
       return confirm(
         'Are you sure you want to continue? Any saved changes will be lost.'
