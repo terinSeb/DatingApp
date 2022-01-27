159b8850b0ecedec063d0927e72f30be0cd86db2
Terin Sebastin <terin01@gmail.com>
2022-01-27 07:20:33 +0530

    Adding the Loading Indicator

--- src/app/_guards/prevent-unsaved-changes.guard.ts
+++ src/app/_guards/prevent-unsaved-changes.guard.ts
@@ -13,7 +13,6 @@ import { MemberEditComponent } from '../members/member-edit/member-edit.componen
 })
 export class PreventUnsavedChangesGuard implements CanDeactivate<unknown> {
   canDeactivate(component: MemberEditComponent): boolean {
-    // console.log(component.editForm.submitted);
     if (component.editForm.dirty) {
       return confirm(
         'Are you sure you want to continue? Any saved changes will be lost.'
