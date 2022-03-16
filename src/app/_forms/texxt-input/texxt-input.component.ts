5f62b09619f5ca9aadd67d4ccc45b6546a7a244f
Terin Sebastin <terin01@gmail.com>
2022-03-16 07:01:01 +0530

    testing and adding reusable text input

--- src/app/_forms/texxt-input/texxt-input.component.ts
+++ src/app/_forms/texxt-input/texxt-input.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Self } from '@angular/core';
+import { ControlValueAccessor, NgControl } from '@angular/forms';
 
 @Component({
   selector: 'app-texxt-input',
   templateUrl: './texxt-input.component.html',
-  styleUrls: ['./texxt-input.component.css']
+  styleUrls: ['./texxt-input.component.css'],
 })
-export class TexxtInputComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class TexxtInputComponent implements ControlValueAccessor {
+  @Input() label: string;
+  @Input() type = 'text';
+  constructor(@Self() public ngControl: NgControl) {
+    this.ngControl.valueAccessor = this;
   }
-
+  writeValue(obj: any): void {
+    throw new Error('Method not implemented.');
+  }
+  registerOnChange(fn: any): void {
+    throw new Error('Method not implemented.');
+  }
+  registerOnTouched(fn: any): void {
+    throw new Error('Method not implemented.');
+  }
 }
